be6d167aceaa5ee0d09139de9e32dc6e7383a661
Aman Dagar <amandagar454@gmail.com>
2026-01-12 16:38:32 +0530

    vercel link deploy

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: "https://gig-flow-15dqkf82p-amandagars-projects.vercel.app/gigs",
   credentials: true
 }));
 
